5b378a75a4d600b478a50c55ef58955858d87ae6
justin <justinvyang@gmail.com>
2018-07-15 17:19:40 -0700

    connected boomtown to postgres database

---
+++ server/config/postgres.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg')
+
+module.exports = function(app) {
+
+  /**
+   * @TODO: Configuration Variables
+   *
+   *  Retrieve the necessary information to connect to Postgres
+   *  For example: app.get('PG_DB')
+   */
+  return new Pool({
+    port: app.get('PORT'),
+    host: app.get('PG_HOST'),
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB'),
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+    /**
+     *  @TODO: Supply the correct configuration values to connect to postgres
+     */
+  })
+}
